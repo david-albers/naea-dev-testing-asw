968ed9ed421dc3fac8c7a826ecfde1d6f0530c29
David Albers <David.Albers@kantar.com>
2022-12-08 17:04:21 -0800

    Adding a try / catch around the getUserGroups call

--- api/aadroles/index.js
+++ api/aadroles/index.js
@@ -1,33 +1,37 @@
-// add role names to this object to map them to group ids in your AAD tenant
-const roleGroupMappings = {
-  "f54690d8-5036-464e-9fd4-ef75b08797f0": "admin",
-  "9035a892-56ff-4271-877f-9d738627226c": "reader",
-};
-
 module.exports = function (context, req) {
-  
+  const roleGroupMappings = {
+    "f54690d8-5036-464e-9fd4-ef75b08797f0": "admin",
+    "9035a892-56ff-4271-877f-9d738627226c": "reader",
+  };
+
   const user = req.body || {};
   const roles = [
     user.userId || "unknown",
   ];
-  getUserGroups(user)
-    .then((resp) => {
+  try {
+    getUserGroups(user)
+      .then((resp) => {
       const usergroups = resp.data.value;
       roles.push.apply(
         roles,
         usergroups
           .filter((grp) => roleGroupMappings.hasOwnProperty(grp.id))
           .map((grp) => grp.displayName)
-      );
-    })
-    .catch((err) => {
-      roles.push(err.response.data.error.message);
-    })
-    .finally(() => {      
-      context.res.json({
-        roles,
+        );
+      })
+      .catch((err) => {
+        roles.push(err.response.data.error.message);
+      })
+      .finally(() => {
+        context.res.json({
+            roles,
+        });
       });
+  } catch {
+    context.res.json({
+      roles,
     });
+  }
 };
 
 function getUserGroups(user) {

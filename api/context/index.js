b69cb9fe07a69c6d3aa06a41221e6650b795c971
David Albers <David.Albers@kantar.com>
2022-12-22 13:28:41 -0800

    Trying to run AAD query in secondary API

--- api/context/index.js
+++ api/context/index.js
@@ -1,8 +1,21 @@
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
 
+  var user = axios({
+    url: `/.auth/me`
+  }).then((res) => res);
+
+  var groups = axios({
+    url: `https://graph.microsoft.com/v1.0/users/${user.userId}/transitiveMemberOf/microsoft.graph.group?$count=true&$select=id,displayName,securityEnabled`,
+    method: "get",
+    timeout: 8000,
+    headers: {
+      Authorization: req.headers.authorization,
+    },
+  }).then((res) => res);
+
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: JSON.stringify(context),
+    body: JSON.stringify(groups),
   };
 };
